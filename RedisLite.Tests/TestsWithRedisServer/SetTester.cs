add11b5b317f34dcab3eda99129a7d012af04aaf
Balázs Kis <19716399+balazs-kis@users.noreply.github.com>
2020-04-05 20:53:15 +0200

    Adding error tests for SetClient

--- RedisLite.Tests/TestsWithRedisServer/SetTester.cs
+++ RedisLite.Tests/TestsWithRedisServer/SetTester.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RedisLite.Client;
+using RedisLite.Client.Exceptions;
 using RedisLite.Tests.TestConfigurations;
+using System;
 using System.Linq;
 
 namespace RedisLite.Tests.TestsWithRedisServer
@@ -26,6 +28,28 @@ namespace RedisLite.Tests.TestsWithRedisServer
         }
 
         [TestMethod]
+        public void TestWrongOperation_SAddThrowsException()
+        {
+            Exception thrownException = null;
+
+            var dut = new RedisClient();
+            dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+
+            try
+            {
+                dut.Set(SetKey, SetValue1);
+                dut.SAdd(SetKey, SetValue2);
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(RedisException));
+        }
+
+        [TestMethod]
         public void Test_SRem()
         {
             var dut = new RedisClient();
@@ -36,6 +60,28 @@ namespace RedisLite.Tests.TestsWithRedisServer
             var result = dut.SMembers(SetKey).ToList();
 
             Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void TestWrongOperation_SRemThrowsException()
+        {
+            Exception thrownException = null;
+
+            var dut = new RedisClient();
+            dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+
+            try
+            {
+                dut.Set(SetKey, SetValue1);
+                dut.SRem(SetKey, SetValue1);
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(RedisException));
         }
 
         [TestMethod]
@@ -53,6 +99,28 @@ namespace RedisLite.Tests.TestsWithRedisServer
         }
 
         [TestMethod]
+        public void TestWrongOperation_SMembersThrowsException()
+        {
+            Exception thrownException = null;
+
+            var dut = new RedisClient();
+            dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+
+            try
+            {
+                dut.Set(SetKey, SetValue1);
+                dut.SMembers(SetKey);
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(RedisException));
+        }
+
+        [TestMethod]
         public void Test_SIsMember()
         {
             var dut = new RedisClient();
@@ -64,6 +132,28 @@ namespace RedisLite.Tests.TestsWithRedisServer
 
             Assert.IsTrue(result1);
             Assert.IsFalse(result2);
+        }
+
+        [TestMethod]
+        public void TestWrongOperation_SIsMemberThrowsException()
+        {
+            Exception thrownException = null;
+
+            var dut = new RedisClient();
+            dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+
+            try
+            {
+                dut.Set(SetKey, SetValue1);
+                dut.SIsMember(SetKey, SetValue1);
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(RedisException));
         }
 
 
