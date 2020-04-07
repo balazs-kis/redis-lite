9688c2bcc41fdef0053df68e67c13c1787279ce1
Balázs Kis <19716399+balazs-kis@users.noreply.github.com>
2020-04-07 16:07:40 +0200

    Tests for public RedisSubscriptionClient

--- RedisLite.Tests/TestsWithRedisServer/SubscriptionTester.cs
+++ RedisLite.Tests/TestsWithRedisServer/SubscriptionTester.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RedisLite.Client;
+using RedisLite.Client.Exceptions;
 using RedisLite.Tests.TestConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace RedisLite.Tests.TestsWithRedisServer
 {
@@ -16,6 +18,229 @@ namespace RedisLite.Tests.TestsWithRedisServer
 
         private const string OtherChannel = "OtherTestChannel";
         private const string OtherMessage = "OtherTestMessage";
+
+        [TestMethod]
+        public void Test_ConnectWithSubscriptionClient()
+        {
+            Exception thrownException = null;
+
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNull(thrownException);
+        }
+
+        [TestMethod]
+        public void TestUnknownHost_ConnectWithSubscriptionClientThrowsException()
+        {
+            Exception thrownException = null;
+
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(UnknownHost.AsConnectionSettings());
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(SocketException));
+        }
+
+        [TestMethod]
+        public void TestKnownHostBadPort_ConnectWithSubscriptionClientThrowsException()
+        {
+            Exception thrownException = null;
+
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(LocalHostPort7000.AsConnectionSettings());
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(SocketException));
+        }
+
+        [TestMethod]
+        public void TestConnectTwice_ConnectWithSubscriptionClientThrowsException()
+        {
+            Exception thrownException = null;
+
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+                dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(InvalidOperationException));
+        }
+
+        [TestMethod]
+        public void TestCallingAfterDispose_SubscriptionClientThrowsException()
+        {
+            Exception thrownException = null;
+
+            var dut = new RedisSubscriptionClient();
+            dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+            dut.Dispose();
+
+            try
+            {
+                dut.Select(1);
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(InvalidOperationException));
+        }
+
+        [TestMethod]
+        public void Test_SelectWithSubscriptionClient()
+        {
+            Exception thrownException = null;
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+                dut.Select(1);
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNull(thrownException);
+        }
+
+        [TestMethod]
+        public void TestWrongDbIndex_SelectWithSubscriptionClientThrowsException()
+        {
+            Exception thrownException = null;
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+                dut.Select(int.MaxValue);
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(RedisException));
+        }
+
+        [TestMethod]
+        public void TestEmptyChannelsList_SubscribeThrowsException()
+        {
+            Exception thrownException = null;
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+                dut.Subscribe(new string[] { });
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(InvalidOperationException));
+        }
+
+        [TestMethod]
+        public void TestAlreadySubscribed_SubscribeThrowsException()
+        {
+            Exception thrownException = null;
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+                dut.Subscribe("channel");
+                dut.Subscribe("other-channel");
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(InvalidOperationException));
+        }
+
+        [TestMethod]
+        public void TestEmptyChannelsList_UnsubscribeThrowsException()
+        {
+            Exception thrownException = null;
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+                dut.Subscribe("channel");
+                dut.Unsubscribe(new string[] { });
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(InvalidOperationException));
+        }
+
+        [TestMethod]
+        public void TestNotSubscribed_UnsubscribeThrowsException()
+        {
+            Exception thrownException = null;
+            var dut = new RedisSubscriptionClient();
+
+            try
+            {
+                dut.Connect(LocalHostDefaultPort.AsConnectionSettings());
+                dut.Unsubscribe("channel");
+            }
+            catch (Exception ex)
+            {
+                thrownException = ex;
+            }
+
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(InvalidOperationException));
+        }
 
         [TestMethod]
         public void Test_Subscribe()
@@ -110,8 +335,6 @@ namespace RedisLite.Tests.TestsWithRedisServer
         [TestMethod]
         public void Test_MultipleUnsubscribe()
         {
-            // TODO: find out why this fails sometimes on CI build
-
             var dutPublisher = new RedisClient();
             var dutSubscriber = new RedisSubscriptionClient();
 
